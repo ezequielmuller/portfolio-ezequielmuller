3380426cc6e889c6916e843768315e471b8f2a25
Ezequiel Muller <zikimuller017@gmail.com>
2025-07-02 19:37:05 -0300

    Menu Principal Funcionando

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,18 +3,21 @@
     <q-header elevated
       style="max-width: 1000px; width: 100%; margin: 0 auto; border-radius: 0 0 20px 20px; overflow: hidden">
       <q-toolbar class="bg-white text-black q-pa-md">
-        <q-btn flat dense round icon="menu" aria-label="Menu" />
-
-        <q-toolbar-title class="text-center text-primary"> Portfólio </q-toolbar-title>
+        <img src="icons/foto_perfil.jpg" alt="Logo" class="q-mr-md"
+          style="width: 60px; height: 60px; border-radius: 30px;" />
+        <q-separator vertical class="q-ml-sm" color="primary" />
+        <q-toolbar-title class="text-justify text-primary"> Ezequiel H. G. Muller </q-toolbar-title>
 
         <div class="row items-center q-gutter-sm">
-          <q-btn flat dense round icon="mdi-whatsapp" aria-label="WhatsApp" @click="goTo('https://wa.me/SEUNUMERO')" />
+          <q-btn flat dense round icon="mdi-whatsapp" aria-label="WhatsApp"
+            @click="goTo('https://wa.me/54999228928')" />
           <q-btn flat dense round icon="mdi-linkedin" aria-label="LinkedIn"
-            @click="goTo('https://www.linkedin.com/in/SEULINK/')" />
+            @click="goTo('https://www.linkedin.com/in/ezequielhgmuller/')" />
           <q-btn flat dense round icon="mdi-instagram" aria-label="Instagram"
-            @click="goTo('https://instagram.com/SEUINSTAGRAM')" />
-          <q-btn flat dense round icon="mdi-github" aria-label="GitHub" @click="goTo('https://github.com/SEUGITHUB')" />
-          <q-btn flat dense round icon="mdi-email" aria-label="Email" @click="goTo('mailto:SEUEMAIL@example.com')" />
+            @click="goTo('https://instagram.com/ezequielmuller__')" />
+          <q-btn flat dense round icon="mdi-github" aria-label="GitHub"
+            @click="goTo('https://github.com/ezequielmuller')" />
+          <q-btn flat dense round icon="mdi-email" aria-label="Email" @click="goTo('mailto:zikimuller017@gmail.com')" />
         </div>
       </q-toolbar>
     </q-header>
